a5fe9f422156f13a4d48b6325bdbce94ccb4300b
Masataro Asai <guicho2.71828@gmail.com>
2021-04-12 09:36:59 -0400

    fix: blocksworld generation: works correctly under parallelism

--- blocks/blocksworld.sh
+++ blocks/blocksworld.sh
@@ -10,8 +10,9 @@ BLOCKS=$2
 BS=./bwstates.1/bwstates
 TR=./${OPS}ops/2pddl/2pddl
 
+states=$(mktemp)
+trap "rm $states" EXIT
+$BS -s 2 -n ${BLOCKS} > $states
+$TR -d $states -n ${BLOCKS}
 
-$BS -s 2 -n ${BLOCKS} > STATES
-$TR -d STATES -n ${BLOCKS}
 
-rm STATES
--- blocks/generate-test-train-instances-pddlrl.sh
+++ blocks/generate-test-train-instances-pddlrl.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-parallel -j 1 "mkdir -pv train/; ./blocksworld.sh 4 {1} > train/o{1}-p{2}.pddl" ::: {02..06} ::: {01..50}
+parallel "mkdir -pv train/; ./blocksworld.sh 4 {1} > train/o{1}-p{2}.pddl" ::: {02..06} ::: {01..50}
 
-parallel -j 1 "mkdir -pv test/; ./blocksworld.sh 4 {1} > test/o{1}-p{2}.pddl" ::: {010..100..10} ::: {001..100}
+parallel "mkdir -pv test/; ./blocksworld.sh 4 {1} > test/o{1}-p{2}.pddl" ::: {010..100..10} ::: {001..100}
