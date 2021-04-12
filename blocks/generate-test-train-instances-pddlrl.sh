9a21f018d74355727d23f5678791c5b5e1856b2c
Masataro Asai <guicho2.71828@gmail.com>
2021-04-12 09:39:26 -0400

    bit of refactoring

--- blocks/generate-test-train-instances-pddlrl.sh
+++ blocks/generate-test-train-instances-pddlrl.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 
-parallel "mkdir -pv train/; ./blocksworld.sh 4 {1} > train/o{1}-p{2}.pddl" ::: {02..06} ::: {01..50}
+set -e
 
-parallel "mkdir -pv test/; ./blocksworld.sh 4 {1} > test/o{1}-p{2}.pddl" ::: {010..100..10} ::: {001..100}
+mkdir -pv train test
+
+parallel "./blocksworld.sh 4 {1} > train/o{1}-p{2}.pddl" ::: {02..06} ::: {01..50}
+
+parallel "./blocksworld.sh 4 {1} > test/o{1}-p{2}.pddl" ::: {010..100..10} ::: {001..100}
