6726e50ed7d15d2bd8664cde51f6750193bc62c8
Masataro Asai <guicho2.71828@gmail.com>
2021-04-18 11:09:39 -0400

    updated miconic instances

--- miconic/generate-test-train-instances-pddlrl.sh
+++ miconic/generate-test-train-instances-pddlrl.sh
@@ -2,15 +2,15 @@
 
 set -e
 
-# rm -rf train test
-# mkdir -pv train test
+rm -rf train test
+mkdir -pv train test
 
 # note: avoiding duplicate instances by hasing values
 
-# parallel 'out=train/p{1}-{2}-{3}.pddl; ./miconic -f {1} -p {2} > $out ; mv $out train/p{1}-{2}-$(md5sum $out | cut -d" " -f1).pddl' ::: {002..004} ::: {002..004} ::: {01..50}
-# 
-# parallel 'mv {} {//}/o$(../count-objects.bin {})-{/}' ::: train/*.pddl
+parallel 'out=train/p{1}-{2}-{3}.pddl; ./miconic -f {1} -p {2} > $out ; mv $out train/p{1}-{2}-$(md5sum $out | cut -d" " -f1).pddl' ::: {002..004} ::: {002..004} ::: {01..50}
 
-parallel 'out=test/p{1}-{2}-{3}.pddl; ./miconic -f {1} -p {2} > $out ; mv $out test/p{1}-{2}-$(md5sum $out | cut -d" " -f1).pddl' ::: {060..120..30} ::: {060..120..30} ::: {01..30}
+parallel 'mv {} {//}/o$(../count-objects.bin {})-{/}' ::: train/*.pddl
+
+parallel 'out=test/p{1}-{2}-{3}.pddl; ./miconic -f {1} -p {2} > $out ; mv $out test/p{1}-{2}-$(md5sum $out | cut -d" " -f1).pddl' ::: {060..120..30} ::: {060..120..30} ::: {01..50}
 
 parallel 'mv {} {//}/o$(../count-objects.bin {})-{/}' ::: test/*.pddl
