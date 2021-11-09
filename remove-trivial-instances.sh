52cb72b76222f0f19169e6b55f44328d6ac68a1e
Masataro Asai <guicho2.71828@gmail.com>
2021-11-09 09:04:49 -0500

    remove-trivial-instances.sh

---
+++ remove-trivial-instances.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+tmp=$(mktemp)
+trap "rm $tmp" EXIT
+export tmp
+
+per-problem (){
+    # if an empty plan file satisfies a goal, then the initial state satisfies a goal
+    if arrival $1 $2 $tmp &>/dev/null
+    then
+        echo "$2 is trivial!"
+        rm -v $2
+    fi
+}
+
+per-domain (){
+    cd $1
+    parallel -j 8 per-problem $(readlink -ef domain.pddl) ::: train/*.pddl
+}
+
+export -f per-domain per-problem
+
+for d in */
+do
+    ( per-domain $d )
+done
